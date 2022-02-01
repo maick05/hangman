5f87eb3b887b85240e3501bcdfda3edf88950bad
Maick Speck <maicksantos05@hotmail.com>
2022-02-01 14:07:12 -0300

    red tests validations integration tests route register

--- src/controllers/__tests__/users/CreateUserTest.js
+++ src/controllers/__tests__/users/CreateUserTest.js
@@ -4,9 +4,13 @@ const HelperString = require("../../../helpers/helperString")
 
 require("dotenv").config()
 const urlBase = process.env.baseUrl
-jest.setTimeout(10000)
+jest.setTimeout(15000)
 
-describe("Route CreateUserService", (done) => {
+afterAll(async () => {
+	await new Promise((resolve) => setTimeout(() => resolve(), 10000)) // avoid jest open handle error
+})
+
+describe("Route /users/register", (done) => {
 	it("Should return status 200, success and the created user id", () => {
 		request.post(
 			{
@@ -30,8 +34,8 @@ describe("Route CreateUserService", (done) => {
 	})
 })
 
-describe("Route CreateUserService Error Name Not Null", () => {
-	it("Should return the created user id", async () => {
+describe("Route /users/register Error Name Not Null", () => {
+	it("Should return the name is_null validation error", async () => {
 		request.post(
 			{
 				url: urlBase + "/users/register",
@@ -46,6 +50,111 @@ describe("Route CreateUserService Error Name Not Null", () => {
 				// expect(response.statusCode).toBe(200)
 				expect(body.success).toBe(false)
 				expect(body.errors).toEqual(expect.arrayContaining(["is_null"]))
+			}
+		)
+	})
+})
+
+describe("Route /users/register Error Email Not Null", () => {
+	it("Should return the email is_null validation error", async () => {
+		request.post(
+			{
+				url: urlBase + "/users/register",
+				form: {
+					name: "Red Test",
+					password: "senhaTeste_" + HelperString.generateRandomString(5)
+				}
+			},
+			function (error, response, body) {
+				body = JSON.parse(body)
+				// expect(response.statusCode).toBe(200)
+				expect(body.success).toBe(false)
+				expect(body.errors).toEqual(expect.arrayContaining(["is_null"]))
+			}
+		)
+	})
+})
+
+describe("Route /users/register Error Password Not Null", () => {
+	it("Should return the pasword is_null validation error", async () => {
+		request.post(
+			{
+				url: urlBase + "/users/register",
+				form: {
+					name: "Red Test",
+					email: "usuario_" + HelperString.generateRandomString(5) + "@test.com"
+				}
+			},
+			function (error, response, body) {
+				body = JSON.parse(body)
+				// expect(response.statusCode).toBe(200)
+				expect(body.success).toBe(false)
+				expect(body.errors).toEqual(expect.arrayContaining(["is_null"]))
+			}
+		)
+	})
+})
+
+describe("Route /users/register Error Name Length", () => {
+	it("Should return the name len validation error", async () => {
+		request.post(
+			{
+				url: urlBase + "/users/register",
+				form: {
+					name: "aa",
+					email:
+						"usuario_" + HelperString.generateRandomString(5) + "@test.com",
+					password: "senhaTeste_" + HelperString.generateRandomString(5)
+				}
+			},
+			function (error, response, body) {
+				body = JSON.parse(body)
+				// expect(response.statusCode).toBe(200)
+				expect(body.success).toBe(false)
+				expect(body.errors).toEqual(expect.arrayContaining(["len"]))
+			}
+		)
+	})
+})
+
+describe("Route /users/register Error Invalid Email", () => {
+	it("Should return the email isEmail validation error", async () => {
+		request.post(
+			{
+				url: urlBase + "/users/register",
+				form: {
+					name: "Test_" + HelperString.generateRandomString(5),
+					email: "usuario_" + HelperString.generateRandomString(5) + "-com",
+					password: "senhaTeste_" + HelperString.generateRandomString(5)
+				}
+			},
+			function (error, response, body) {
+				body = JSON.parse(body)
+				// expect(response.statusCode).toBe(200)
+				expect(body.success).toBe(false)
+				expect(body.errors).toEqual(expect.arrayContaining(["isEmail"]))
+			}
+		)
+	})
+})
+
+describe("Route /users/register Error Password Length", () => {
+	it("Should return the password len validation error", async () => {
+		request.post(
+			{
+				url: urlBase + "/users/register",
+				form: {
+					name: "aa",
+					email:
+						"usuario_" + HelperString.generateRandomString(5) + "@test.com",
+					password: "senhaTeste_" + HelperString.generateRandomString(5)
+				}
+			},
+			function (error, response, body) {
+				body = JSON.parse(body)
+				// expect(response.statusCode).toBe(200)
+				expect(body.success).toBe(false)
+				expect(body.errors).toEqual(expect.arrayContaining(["len"]))
 			}
 		)
 	})
